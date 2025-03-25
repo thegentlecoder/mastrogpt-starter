2bc4af5acb0dc2a61c7f927620d6944a2d9bdb59
thegentlecoder <33804706+thegentlecoder@users.noreply.github.com>
2025-03-25 21:45:46 +0000

    Ex 5 - Save image

--- packages/vision/form/form.py
+++ packages/vision/form/form.py
@@ -1,14 +1,11 @@
 import os, requests as req
-import sys, pathlib, time
-#sys.path.append("packages/vision/store")
+import time
 import base64
 import vision
 import bucket
 
 USAGE = """
-Please upload a picture and I will tell you what I see, or
-@<substring>      decode files starting with <substring>
-?                 this message
+Please upload a picture and I will save it and tell you what I see
 """
 FORM = [
   {
@@ -27,7 +24,6 @@ def form(args):
   if type(inp) is dict and "form" in inp:
     img = inp.get("form", {}).get("pic", "")
     print(f"uploaded size {len(img)}")
-    #print(f"img {img}")
     vis = vision.Vision(args)
     out = vis.decode(img)
     res['html'] = f'<img src="data:image/png;base64,{img}">'
@@ -36,8 +32,6 @@ def form(args):
     filename = "upload/image_" + timestr + ".jpg"
     out += "\n\n" + filename
     body = base64.b64decode(img)
-    print(f"filename {filename}")
-    print(f"body {body}")
     try:
       buc = bucket.Bucket(args)
       buc.write(filename, body)
--- tests/vision/test_store.py
+++ tests/vision/test_store.py
@@ -1,20 +1,22 @@
 import sys, pathlib
 sys.path.append("packages/vision/store")
 import bucket, vision
+import base64
 
 def test_store():
     buc = bucket.Bucket({})
     assert len(buc.find("cat")) == 0
     body = pathlib.Path("tests/vision/cat.jpg").read_bytes()
+    print(f"body {body}")
     buc.write("cat.jpg", body)
     ls = buc.find("cat")
     assert len(ls) == 1
     sz =  buc.size(ls[0])
     assert sz > 0
-    b64 = buc.read_b64(ls[0])
+    b64 = buc.read(ls[0])
     assert len(b64) >= sz
     vis = vision.Vision({})
-    res = vis.decode(b64)
+    res = vis.decode(base64.b64encode(b64).decode("utf-8"))
     assert res.find("cat") != -1
     n = buc.remove(ls[0])
     assert n == 1

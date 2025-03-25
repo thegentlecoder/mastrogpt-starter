5072cb3c9fdc0ac82dfe012a24a4315b85ecfb3f
thegentlecoder <33804706+thegentlecoder@users.noreply.github.com>
2025-03-25 22:47:53 +0000

    Ex 5 - Done - Use exturl

--- packages/vision/form/form.py
+++ packages/vision/form/form.py
@@ -26,20 +26,18 @@ def form(args):
     print(f"uploaded size {len(img)}")
     vis = vision.Vision(args)
     out = vis.decode(img)
-    res['html'] = f'<img src="data:image/png;base64,{img}">'
+    #res['html'] = f'<img src="data:image/png;base64,{img}">'
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     filename = "upload/image_" + timestr + ".jpg"
     out += "\n\n" + filename
     body = base64.b64decode(img)
-    try:
-      buc = bucket.Bucket(args)
-      buc.write(filename, body)
-    except Exception as err:
-      exc_type, exc_obj, exc_tb = sys.exc_info()
-      fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-      out += f"\n\nUnexpected {err=}, {type(err)=}, {exc_type=}, {fname=}, {exc_tb.tb_lineno=}"
-      
+    buc = bucket.Bucket(args)
+    buc.write(filename, body)
+    url = buc.exturl(filename, 3600)
+    print(url)
+    res['html'] = f"<img src='{url}'>"
+
   res['form'] = FORM
   res['output'] = out
   return res
